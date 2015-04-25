9881d0603f2dba89f90f68a1ec1e4bb0fcc8be73
mingcaoyouxin <mingcaoyouxin@gmail.com>
2015-04-25 16:32:26 +0800

    change

--- MyJava/SingletonDesign/src/SafeSingletonStaticNested.java
+++ MyJava/SingletonDesign/src/SafeSingletonStaticNested.java
@@ -3,12 +3,14 @@
  *  使用内部静态类的单例模式
  */
 public class SafeSingletonStaticNested {
-    private static class SingletonHolder{
-        private static final SafeSingletonStaticNested INSTANCE =new SafeSingletonStaticNested();
-    }
-    private SafeSingletonStaticNested(){
 
-    }
+   private static class SingletonHolder{
+       private final static SafeSingletonStaticNested INSTANCE=new SafeSingletonStaticNested();
+   }
+
+    private SafeSingletonStaticNested(){}
+
+    //静态方法
     public static final SafeSingletonStaticNested getInstance(){
         return SingletonHolder.INSTANCE;
     }
