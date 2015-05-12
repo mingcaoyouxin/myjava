f6eddedd0b57b1621141b64bb0c4d72ffd7b06ff
mingcaoyouxin <990241026@qq.com>
2015-05-12 15:26:53 +0800

    new class

---
+++ MyJava/ObjectShared/src/Secret.java
@@ -0,0 +1,5 @@
+/**
+ * Created by pc on 15-5-12.
+ */
+public class Secret {
+}
---
+++ MyJava/ObjectShared/src/Test.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+/**
+ * Created by pc on 15-5-12.
+ */
+public class Test {
+    public static Set<Secret> knownSecrets;
+
+    public void initialize(){
+        knownSecrets=new HashSet<Secret>();
+    }
+}
